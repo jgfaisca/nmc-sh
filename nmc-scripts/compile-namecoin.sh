b22861a3eb90d5a0ba95ffc6574ce0965ddf50fb
zekaf <jose.faisca@gmail.com>
2016-05-19 21:00:30 +0100

    Update compile-namecoin.sh

--- nmc-scripts/compile-namecoin.sh
+++ nmc-scripts/compile-namecoin.sh
@@ -3,9 +3,9 @@
 # install nmc from source
 # https://wiki.namecoin.org/index.php?title=Build_Namecoin_From_Source
 #
-sudo apt-get install libtool git
+sudo apt-get install libtool git libdb++-dev
 git clone https://github.com/namecoin/namecoin-core.git
 cd namecoin-core
 ./autogen.sh
-./configure --without-gui
+./configure --without-gui --with-incompatible-bdb
 make
