17b7a4b39f9c5d984b22379e79ba5d7ea99be11b
zekaf <jose.faisca@gmail.com>
2016-08-20 16:59:37 +0100

    Create build-nmc-node-nowallet.sh

---
+++ nmc-scripts/build-nmc-node-nowallet.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Build and install Namecoin node from source (without wallet)
+# https://github.com/namecoin/namecoin-core/blob/master/doc/build-unix.md
+#
+
+# -- Install main depencencies --
+apt-get update && apt-get install -y libboost-all-dev \
+        dh-autoreconf curl libcurl4-openssl-dev \
+        git apg libboost-all-dev build-essential libtool \
+        libevent-dev wget bsdmainutils autoconf \
+        apg libqrencode-dev libcurl4-openssl-dev \
+        automake make libssl-dev libminiupnpc-dev \
+        pkg-config libdb++-devv libzmq3-dev
+
+# -- Clone Namecoin source repository --
+[ -d "namecoin-core" ] || git clone https://github.com/namecoin/namecoin-core.git
+
+# -- Compile namecoin --
+cd namecoin-core
+./autogen.sh
+# CXX flags tuned to conserve memory
+CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768"
+./configure \
+        CXXFLAGS="$CXXFLAGS" \
+        --enable-cxx \
+        --disable-shared \
+        --with-pic \
+        --without-gui \
+        --enable-upnp-default \
+        --disable-wallet
+make && make install
+
+# -- Clean --
+cd / \
+        && apt-get autoremove -y \
+        && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
