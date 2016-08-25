5f72b321e165666a73f6d5b0ca4a56a806823533
zekaf <jose.faisca@gmail.com>
2016-08-25 13:43:27 +0100

    Create test-rpc-connection.sh

---
+++ nmc-scripts/test-rpc-connection.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Test Namecoin RPC connection on localhost
+# 
+# usage: 
+# ./test-rpc-connection <data_directory>
+#
+# example:
+# ./test-rpc-connection /home/myuser/.namecoin
+#
+
+if [ $# -ne 1 ]; then
+  echo
+  echo "Invalid number of arguments."
+  echo "Usage: ./$(basename "$0") <data_directory>"
+  echo
+  exit 1
+fi
+
+DATADIR=$1
+CONF=namecoin.conf
+HOST="127.0.0.1"
+
+RPC_USER=$(cat $DATADIR/$CONF | grep rpcuser | awk -F '[/=]' '{print $2}')
+RPC_PASS=$(cat $DATADIR/$CONF | grep rpcpassword | awk -F '[/=]' '{print $2}')
+RPC_PORT=$(cat $DATADIR/$CONF | grep rpcport | awk -F '[/=]' '{print $2}')
+
+curl \
+        --user $RPC_USER:$RPC_PASS \
+        --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getinfo","params":[]}' \
+        -H 'content-type: text/plain;' \
+        http://$HOST:$RPC_PORT
