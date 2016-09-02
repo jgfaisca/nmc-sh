0513bc5592a2a42f292eccb999ba07617f2f7c02
zekaf <jose.faisca@gmail.com>
2016-09-02 01:44:22 +0100

    Create dump-private-key.sh

---
+++ nmc-scripts/dump-private-key.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#########################################################
+#
+# Dump private key
+#
+# usage:
+# ./dump-private-key <namecoin_address>
+#
+# example:
+# ./dump-private-key NEv8dd3sxN7gV7RnWy9VefPqWswxhbc28c
+#
+# authors:
+# jose G. Faisca <jose.faisca@gmail.com>
+#
+#########################################################
+
+
+if [ $# -ne 1 ]; then
+  echo
+  echo "Invalid number of arguments."
+  echo "Usage: ./$(basename "$0") <namecoin_address>"
+  echo
+  exit 1
+fi
+
+DATADIR=/data/namecoin
+CONF=namecoin.conf
+HOST="127.0.0.1"
+
+RPC_USER=$(cat $DATADIR/$CONF | grep rpcuser | awk -F '[/=]' '{print $2}')
+RPC_PASS=$(cat $DATADIR/$CONF | grep rpcpassword | awk -F '[/=]' '{print $2}')
+RPC_PORT=$(cat $DATADIR/$CONF | grep rpcport | awk -F '[/=]' '{print $2}')
+
+function call(){
+  method="$1"
+  params="$2"
+  return=$(curl -D -sS --user ${RPC_USER}:${RPC_PASS} \
+        --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"'"${method}"'","params":["'"${params}"'"]}' \
+        -H 'content-type: text/plain;' \
+        http://${HOST}:${RPC_PORT})
+  echo $return
+}
+
+function print_json(){
+  json_pp -v >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+     echo "$1"
+  else
+     echo "$1" | json_pp
+  fi
+}
+
+print_json $(call dumpprivkey "$1")
