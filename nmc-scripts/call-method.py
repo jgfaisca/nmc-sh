fdd5bd906966010ea6049e453e3e01954053a5ff
zekaf <jose.faisca@gmail.com>
2019-02-17 04:48:09 +0000

    update

--- nmc-scripts/call-method.py
+++ nmc-scripts/call-method.py
@@ -1,9 +1,25 @@
-# Generic JSON-RPC call (python)
+#!/usr/bin/python
+# dependencies:
+# pip install python-bitcoinrpc
+#
 
-headers = {
-    'content-type': 'text/plain;',
-}
+rpc_user="rpc"
+rpc_password="secret"
 
-data = '{"jsonrpc":"1.0","id":"curltext","method":"${method}","params":["${params}"]}'
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
-requests.post('http://${HOST}:${RPC_PORT}', headers=headers, data=data)
+# rpc_user and rpc_password are set in the bitcoin.conf file
+rpc_connection = AuthServiceProxy("http://%s:%s@10.17.0.2:8336"%(rpc_user, rpc_password))
+
+#best_block_hash = rpc_connection.getbestblockhash()
+#print(rpc_connection.getblock(best_block_hash))
+# batch support : print timestamps of blocks 0 to 99 in 2 RPC round-trips:
+#commands = [ [ "getblockhash", height] for height in range(100) ]
+#block_hashes = rpc_connection.batch_(commands)
+#blocks = rpc_connection.batch_([ [ "getblock", h ] for h in block_hashes ])
+#block_times = [ block["time"] for block in blocks ]
+#print(block_times)
+
+json_data = rpc_connection.getblockchaininfo()
+print (json_data)
+
